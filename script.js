d6a38c3e5de8c5750950dd95a06946927f7d0f3e
Git-sach <86381409+Git-sach@users.noreply.github.com>
2022-10-13 18:21:25 +0200

    2 files modified

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 console.log('Hello!');
 
 let button = document.querySelector('.button');
+let result = document.querySelector('.result');
 
 button.addEventListener('click', () => {
-  console.log(document.querySelector('.input').value);
+  let value = document.querySelector('.input').value;
+  console.log(value.replace(/\n\r?/g, '<br>'));
+  result.innerHTML = value.replace(/\n\r?/g, '<br>');
 });
